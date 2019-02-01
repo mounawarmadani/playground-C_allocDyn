cf9d2fc995e51e8bfb6a04b515c1ac739dc959e4
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-02-01 21:03:01 +0100

    ex4

---
+++ fusion/fusion.h
@@ -0,0 +1,4 @@
+int* fusion(int *vect1, int *vect2) {
+	//Ecrire le code de la fonction
+	return 0;
+}
---
+++ fusion/test_fusion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int *v1,*v2, *v;
+  int i,n1,n2;
+
+  scanf("%d",&n1);
+  v1=malloc(sizeof(int)*n1);
+  for (i=0;i<n1;i++)
+	  scanf("%d",v1+i);
+
+  scanf("%d",&n2);
+  v2=malloc(sizeof(int)*n1);
+  for (i=0;i<n2;i++)
+	  scanf("%d",v2+i);  
+
+  v=fusion(v1,v2);
+  
+  for (i=0;i<n1+n2;i++)
+	  printf("%d ",v+i);  
+  
+  printf("\n");
+  return 0;
+}
