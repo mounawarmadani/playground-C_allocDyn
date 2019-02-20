0dfd50a2581f9934392b6f97bf78980027788233
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-02-20 09:37:11 +0100

    Update test_fibo.c

--- fibo/test_fibo.c
+++ fibo/test_fibo.c
@@ -3,7 +3,10 @@
 #include "fibo.h"
 
 int _fib(int n) {
-	if (n==0 || n==1) return 1;
+	
+	if (n==0) return 0;
+	else if (n==1) return 1; 
+	else if (n==2) return 1; 
 	else return _fib(n-1)+_fib(n-2);
 }
 
